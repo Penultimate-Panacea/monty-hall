9aca96b458eac715e2e43170bea226b632a7902b
Rian Fantozzi <rian@fantozzi.org>
2022-10-12 16:30:00 -0400

    User Input Added

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
+use std::i32;
 use std::io::Read;
-
-// use percentage::Percentage;
-// use percentage::PercentageDecimal;
-// use rand::Rng;
 use rand::distributions::Uniform;
 use rand::distributions::Distribution;
 use rand::rngs::ThreadRng;
@@ -102,18 +99,37 @@ fn simulation(num_doors: i64, num_simulations: u32, change_choice: bool) -> frac
 }
 
 
+use simple_user_input::get_input;
+
+// SOURCE: https://users.rust-lang.org/t/how-to-get-user-input/5176/7
+mod simple_user_input {
+    use std::io;
+    pub fn get_input(prompt: &str) -> String{
+        println!("{}",prompt);
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_goes_into_input_above) => {},
+            Err(_no_updates_is_fine) => {},
+        }
+        input.trim().to_string()
+    }
+}
+
 fn main() {
     env_logger::init();
     println!("Suppose you're on a game show, and you're given the choice of three doors: Behind one door is a car; behind the others, goats.");
     println!("You pick a door, say No. 1, and the host, who knows what's behind the doors, opens another door, say No. 3, which has a goat.");
     println!("She then says to you, \"Do you want to pick door No. 2?\" Is it to your advantage to switch your choice?");
     play_goat_bleet();
-    println!("Win rate: {}", simulation(3, i16::MAX as u32 , false)); 
-    println!("Win rate: {}", simulation(3, i16::MAX as u32, true)); 
-    println!("Win rate: {}", simulation(5, i16::MAX as u32 , false)); 
-    println!("Win rate: {}", simulation(5, i16::MAX as u32, true)); 
-    println!("Win rate: {}", simulation(100, i16::MAX as u32, false)); 
-    println!("Win rate: {}", simulation(100, i16::MAX as u32, true)); 
+    
+    let input_string:String = get_input("Enter number of simulations: MAX VALUE {2,147,483,647}");
+    let input_int: i32 = input_string.parse::<i32>().unwrap();
+    println!("3 Doors, No Change Win rate: {:#.3}", simulation(3, input_int as u32 , false)); 
+    println!("3 Doors,  Change Win rate: {:#.3}", simulation(3, input_int as u32, true)); 
+    println!("5 Doors, No Change Win rate: {:#.3}", simulation(5, input_int as u32 , false)); 
+    println!("5 Doors,  Change Win rate: {:#.3}", simulation(5, input_int as u32, true)); 
+    println!("100 Doors, No Change Win rate: {:#.3}", simulation(100, input_int as u32, false)); 
+    println!("100 Doors,  Change Win rate: {:#.3}", simulation(100, input_int as u32, true)); 
     println!("Press ENTER to continue...");
     let buffer = &mut [0u8];
     std::io::stdin().read_exact(buffer).unwrap();
