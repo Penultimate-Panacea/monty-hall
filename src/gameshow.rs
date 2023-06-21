26a7146088633d5f344830662d28db02a814e12b
Rian Fantozzi <rian@fantozzi.org>
2023-06-21 11:10:33 -0400

    Seperated gameshow into its own file for ease

---
+++ src/gameshow.rs
@@ -0,0 +1,19 @@
+///
+/// Runs a single game of the game show, the participant chooses a door within this function and the function returns if the participant wins or not.
+/// While https://xkcd.com/1282 makes excellent points, here we take the original conceptualization of the problem where winning a goat is considered negative
+///  # Arguments
+/// 
+///  * door_vec:vec = List of Doors
+///  * change: bool = Whether or not the participant will change their choice
+/// 
+/// # Return
+///  
+/// * winner:bool = Whether or not the participant won the prize in this game.
+/// 
+///  # Todo
+///  Maybe take an argument for different goat bleets?
+/// 
+///
+fn run_game (door_vec:Vec<Door>, change:bool) -> bool {
+	true
+}
